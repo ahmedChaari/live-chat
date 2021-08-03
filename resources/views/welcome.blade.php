3538a8191ac8ffe6ff70de699702b5e08e97fa37
ahmedChaari <ahmed17ch@gmail.com>
2021-08-03 23:27:50 +0100

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
             }
         </style>
     </head>
-    <body class="antialiased">
+    <body class="antialiased" style="">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -36,7 +36,6 @@
                     @endauth
                 </div>
             @endif
-
             <h1>MindCom CHAT</h1>
         </div>
     </body>
